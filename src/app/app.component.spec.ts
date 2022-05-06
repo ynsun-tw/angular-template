22dcd5eb5a70b92beb51edbc58bb244394b1f6fd
ynsun <ynsun@thoughtworks.com>
2022-05-06 12:02:14 +0800

    Initial angular project with jest, material, ngrx

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,19 +14,19 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  test('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'price-point-studio'`, () => {
+  test(`should have as title 'price-point-studio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('price-point-studio');
   });
 
-  it('should render title', () => {
+  test('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
